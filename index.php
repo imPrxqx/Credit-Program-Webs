3b89c8bda51ad601a9bdbe0c5264886d460b68e9
imPrxqx <77047836+imPrxqx@users.noreply.github.com>
2024-02-08 19:31:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
 
-$request_uri = $_SERVER['REQUEST_URI'];
-
-if ($request_uri === '/articles') {
-	
-	require_once('Controller/ShowArticleController.php');
-	$showArticlesController = new ShowArticlesController();
-	$showArticlesController->process();
-	
-} elseif (preg_match('~^/article/(\d+)$~', $request_uri, $matches)) {
-	
-	require_once('Controller/DetailArticleController.php');
-	$detailArticleController = new DetailArticleController($matches[1]);
-	$detailArticleController->process();
-	
-} elseif (preg_match('~^/article-edit/(\d+)$~', $request_uri, $matches)) {
-	
-	require_once('Controller/EditArticleController.php');
-	$editArticleController = new EditArticleController($matches[1]);
-	$editArticleController->process();
-	
-} else {
-	
-	require_once('View/ArticleView.php');
-	$articleView = new ArticleView();
-	$articleView->renderView("error", "403 - NOT FOUND PAGE!!!", "error", 403);
-	
-}
-
-/*
-
-
 $request_uri = $_GET['page'];
 
 if ($request_uri === 'articles') {
@@ -58,5 +27,3 @@ if ($request_uri === 'articles') {
 	$articleView->renderView("error", "403 - NOT FOUND PAGE!!!", "error", 403);
 	
 }
-
-*/
